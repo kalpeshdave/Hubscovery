d8e25b428b4fa13d8f6021a8aaf268dc2e08c567
Jeff Schoolcraft <jeff@thequeue.net>
2011-05-11 09:48:03 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instanthub::Application.config.secret_token = '8b77be99212900c47ef2da80a35fe5267d039c09c46d35e6badd13308d6a1e80732e2cb1a13adc07833b516c1776e32a9958c214a3fe9818c8a4caa835907179'
